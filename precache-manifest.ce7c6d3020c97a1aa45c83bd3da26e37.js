304042803ba781cc163c67a8ed26787a8a95ef38
Caio Rosa <caiopetruccirosa@gmail.com>
2020-10-12 20:09:47 -0300

    Updates

---
+++ precache-manifest.ce7c6d3020c97a1aa45c83bd3da26e37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67bb00812239706acd981bed4c71bd23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c6fe132151f0a73edb0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "024469c6845489668bbc",
+    "url": "/static/js/2.a0c15223.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a0c15223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6fe132151f0a73edb0",
+    "url": "/static/js/main.8d05dd3b.chunk.js"
+  },
+  {
+    "revision": "ff66f1e87091aa7ab295",
+    "url": "/static/js/runtime-main.5666167c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
