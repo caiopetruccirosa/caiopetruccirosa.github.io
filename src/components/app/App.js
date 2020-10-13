cb09fac500ed8a9797036a4e5d5765aadee8831b
Caio Rosa <caiopetruccirosa@gmail.com>
2020-10-12 21:47:23 -0300

    Changed App to class extending React.Component

---
+++ src/components/app/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            I'm still working on it!
+          </p>
+
+          <h3>Coming soon...</h3>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
